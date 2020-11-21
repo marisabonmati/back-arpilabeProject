6b797002bb4b4b6b5496a5d15cc71ce771c21b5e
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-21 21:06:13 +0100

    DB connecter

---
+++ AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using back_arpilabeProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_arpilabeProject
+{
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Contact> Contact { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=APContacts;Uid=root;Pwd=Marisa90");
+        }
+
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace back_arpilabeProject.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public int telephone { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+
+        public string Prenom { get; set; }
+        [Required]
+
+        public string Email { get; set; }
+        [Required]
+
+        public string Departement { get; set; }
+        [Required]
+
+        public string Note { get; set; }
+        [Required]
+
+        public DateTime DateDeNaissance { get; set; }
+
+    }
+}
